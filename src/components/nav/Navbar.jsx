84dbb785f949d8f1fff1e8eeb12d0e547747588d
djk239 <dale.king@rutgers.edu>
2023-12-09 23:49:14 -0500

    inital revamp upload

---
+++ src/components/nav/Navbar.jsx
@@ -0,0 +1,27 @@
+import "./Navbar.css"
+
+function Navbar() {
+
+  return (
+    <>
+    <div className='navcontain'>
+    <div className="logo">
+        <h1 className="n1">N</h1>
+        <h1 className="n2">N</h1>
+    </div>
+    <h2 className='nothing'>NOTHING</h2>
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" fill="white">
+  <circle cx="12" cy="6" r="2"/>
+  <circle cx="12" cy="12" r="2"/>
+  <circle cx="12" cy="18" r="2"/>
+</svg>
+
+
+    </div>
+    <div className='bar2'/>
+
+    </>
+  )
+}
+
+export default Navbar
