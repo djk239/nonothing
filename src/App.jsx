c8c728d66c0dd27af24b55460f2040a89b707c2f
djk239 <dale.king@rutgers.edu>
2023-12-11 01:40:28 -0500

    added prompt cyclying. accepts answers

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import './App.css'
 import Navbar from './components/nav/Navbar'
 import Prompt from './components/prompts/Prompt'
 import Footer from './components/Footer/Footer'
+import promptData from "./assets/prompts.json"
 
 function App() {
   return (
     <div className="app-container">
       <Navbar />
       <main>
-        <Prompt />
+        <Prompt prompt={promptData.prompts} />
       </main>
       <Footer />
     </div>
--- src/components/prompts/Prompt.jsx
+++ src/components/prompts/Prompt.jsx
@@ -1,18 +1,31 @@
 import styles from "./Prompt.module.css"
 import { useState } from "react"
 
-function Prompt() {
+
+function Prompt({ prompt }) {
 const [answer, setAnswer] = useState("")
+
+const [promptIndex, setPrompt] = useState(0);
+const nextPrompt= () => {
+  if(answer === prompt[promptIndex].answer){
+    setPrompt((prevIndex) => (prevIndex +1) %prompt.length);
+    setAnswer("");
+  }
+};
 
   return (
     <>
     <div className={styles.container}>
-        <label for="answer" className={styles.prompt}>How many N's are on this screen?</label>
+      <div className={styles.promptWrap} >
+        <label htmlFor="answer">{prompt[promptIndex].prompt}</label>
+        <h3 htmlFor="answer">{prompt[promptIndex].subprompt}</h3>
+        {prompt[promptIndex].url !== "" && (<a target="_blank" rel="noopener noreferrer" href={prompt[promptIndex].url}>this</a>)}
+      </div>
         <input id="answer" type="text" className={styles.input} value={answer} onChange={ (e) => setAnswer(e.target.value)}/>
-        <button className={styles.btn}>Submit</button>
+        <button className={styles.btn} onClick={nextPrompt}>Submit</button>
     </div>
     </>
   )
-}
+};
 
 export default Prompt
