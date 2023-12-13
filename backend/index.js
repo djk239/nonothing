5dbb3e2591c243d42197884162f3033bf5fa65ad
djk239 <dale.king@rutgers.edu>
2023-12-13 15:59:52 -0500

    backend work and mobile updates

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,42 @@
 const express = require('express');
-const mysql = require('mysql');
+const mysql = require('mysql2');
+const cors = require('cors');
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 const db = mysql.createConnection({
-    user: 'root',
     host: 'localhost',
-    password: "password",
-    database: "LoginSystem",
-});
+    user: 'root',
+    password: 'corydale18$',
+    database: 'temp',
+  });
+  
+  db.connect((err) => {
+    if (err) {
+      console.error('Error connecting to database:', err);
+      return;
+    }
+    console.log('Database connected successfully!');
+  });
+
+app.post('/register', (req, res) => {
+
+    const username = req.body.USERNAME
+    const password = req.body.PASSWORD
+
+
+
+    db.query("INSERT INTO tempusers (username, password) VALUES (?,?)",
+     [username, password],
+     (err, result) => {
+        console.log(err);
+    });
+}); 
+
+app.listen(3001, () => {
+    console.log("running server");
+})
 
--- src/popup/Menu.jsx
+++ src/popup/Menu.jsx
@@ -1,7 +1,8 @@
 import { useState } from "react"
 import styles from "./Menu.module.css"
+import Axios from "axios";
 
-function Menu() {
+function Menu( { close } ) {
 const [isLoggingin, setisLoggingin] = useState(true);
 const [username, setUsername] = useState("");
 const [password, setPassword] = useState("");
@@ -11,52 +12,64 @@ const [email, setEmail] = useState("");
 const handleSignin = () => {
     setisLoggingin(!isLoggingin)
 }
+
+const register = (event) =>{
+    event.preventDefault();
+    Axios.post('http://localhost:3001/register', {USERNAME: username,
+                                                  PASSWORD: password}).then((response) => {
+                                                    console.log(response);
+                                                  });
+
+};
+
+const closeMenu = () => {
+    close();
+}
+
   return (
     <>
-        <div className={styles.container}>
-            <h2 className={styles.head}>Login / Signup</h2>
+        <div className={`${styles.container} ${isLoggingin ? styles.login : styles.signup}`}>
+            <div className={styles.topbar}>
+                <h2 className={styles.head}>Login / Signup</h2>
+                <img onClick={closeMenu} src="src\assets\xsvg.svg" alt="close menu" />
+            </div>
             {isLoggingin ? (
-            <div>
+            <div> 
             <h3>Login</h3>
             <ul className={styles.loginlist}>
                 <li>
                     <label htmlFor="usernameL">Username</label>
-                    <input id="usernameL" type="text" />
+                    <input id="usernameL" type="text" value={username} onChange={ (e) => setUsername(e.target.value)}/>
                 </li>
                 <li>
                     <label htmlFor="passwordL">Password</label>
-                    <input id="passwordL" type="password" />
-                </li>
-                <li>
-                    <button>Login</button>
-                    <button onClick={handleSignin}>Or Signup</button>
+                    <input id="passwordL" type="password" value={password} onChange={ (e) => setPassword(e.target.value)} />
                 </li>
             </ul>
             </div>
             ) : (
             <div>
-            <h3>SignUp</h3>
+            <h3 className={styles.head2}>Sign Up</h3>
             <ul className={styles.signuplist}>
                 <li>
                     <label htmlFor="email">Email</label>
-                    <input id="email" type="email" />
+                    <input id="email" type="email" value={email} onChange={ (e) => setEmail(e.target.value)} />
                 </li>
                 <li>
                     <label htmlFor="usernameS">Username</label>
-                    <input id="usernameS" type="text" />
+                    <input id="usernameS" type="text" value={username} onChange={ (e) => setUsername(e.target.value)} />
                 </li>
                 <li>
                     <label htmlFor="passwordS">Password</label>
-                    <input id="passwordS" type="password" />
-                </li>
-                <li>
-                    <button>Signup</button>
-                    <button onClick={handleSignin}>Or Login</button>
+                    <input id="passwordS" type="password" value={password} onChange={ (e) => setPassword(e.target.value)} />
                 </li>
             </ul>
             </div>
             )}
-
+                    <div className={styles.btnwrap}>
+                        <button className={styles.btn} onClick={register}>Login</button>
+                        <button className={styles.btn} onClick={handleSignin}>Switch</button>
+                    </div>
         </div>
     </>
   )
